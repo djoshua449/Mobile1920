67a03c6dd811abec18bb9e0e178ee2f23d2a2e77
Dave <djoshua449@gmail.com>
2019-09-27 01:51:49 +0700

    The Adventurer - Week 5

---
+++ The Adventurer - week 5/app/src/main/java/com/github/djoshua449/theAdventurer/MainMenu.kt
@@ -0,0 +1,26 @@
+package com.github.djoshua449.theAdventurer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.github.djoshua449.theAdventurer.databinding.FragmentMainMenuBinding
+
+class MainMenu : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentMainMenuBinding>(inflater,
+            R.layout.fragment_main_menu, container, false)
+
+        // Listeners ...
+        binding.startButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_mainMenu_to_gameFragment)
+        }
+
+        return binding.root
+    }
+
+}
